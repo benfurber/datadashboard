5a96d941c051e0d534b0e8f1ed9d615b0d9d7cd9
benfurber <ben@furber-consultancy.com>
2017-11-21 08:20:18 +0000

    An actual working transformation script

--- js/summary.js
+++ js/summary.js
@@ -1,17 +1,30 @@
-'use strict';
+//require the csvtojson converter class
+var Converter = require("csvtojson").Converter;
+// create a new converter object
+var converter = new Converter({});
 
-const fs = require('fs');
-const parse = require('csv-parse');
-const transform = require('stream-transform');
+// call the fromFile function which takes in the path to your
+// csv file as well as a callback function
+converter.fromFile("../data/canvas1-processed.csv",function(err,result){
+    // if an error has occured then handle it
+    if(err){
+        console.log("An Error Has occured");
+        console.log(err);
+    }
+    // create a variable called json and store
+    // the result of the conversion
+    var json = result;
 
-const parser = parse({delimiter: ','})
+    console.log(json[0].regID);
 
-const output = [];
-const input = fs.createReadStream('../data/canvas1-processed.csv');
+    for (var i = 0, len = json.length; i < len; i++) {
+      json[i] = {
+        regID : json[i].regID,
+        supporterType: json[i].supporterType,
+        theChannel: json[i].theChannel
+      };
+    }
 
-var transformer = transform(function(record, callback){
-  setTimeout(function(){
-    callback(null, record.join(' ')+'\n');
-  }, 500);
-}, {parallel: 10});
-input.pipe(parser).pipe(transformer).pipe(process.stdout);
+    // log our json to verify it has worked
+    console.log(json);
+});
