a0ed887076d1e896b91a4b4c92a057926bdd35aa
benfurber <ben@furber-consultancy.com>
2017-11-22 07:17:28 +0000

    Success working with JS for loops

--- js/summary.js
+++ js/summary.js
@@ -8,14 +8,35 @@ var converter = new Converter({});
 converter.fromFile("../data/canvas1-processed.csv",function(err,result){
     // if an error has occured then handle it
     if(err){
-        console.log("An Error Has occured");
+        console.log("An error Has occured");
         console.log(err);
     }
     // create a variable called json and store
     // the result of the conversion
     var json = result;
 
-    console.log(json[0].regID);
+    // The supporter types and channels
+    var supporterTypes = ["Cold", "Warm", "Loyal"];
+    var channelTypes = ["Email", "PPC", "Seach", "Social"];
+
+    // Building the JSON structure
+
+    var forExport = {
+      "Cold": {
+        "search": 0,
+        "social": 0,
+      },
+      "Warm": {
+        "search": 0,
+        "social": 0,
+      },
+      "Loyal": {
+        "search": 0,
+        "social": 0,
+      }
+    };
+
+    console.log(forExport["Cold"].search);
 
     for (var i = 0, len = json.length; i < len; i++) {
       json[i] = {
@@ -23,7 +44,14 @@ converter.fromFile("../data/canvas1-processed.csv",function(err,result){
         supporterType: json[i].supporterType,
         theChannel: json[i].theChannel
       };
+      if (json[i].supporterType == "Cold") {
+        if (json[i].theChannel == "Search") {
+          forExport["Cold"].search += 1
+        };
+      }
     }
+
+    console.log(forExport);
 
     // log our json to verify it has worked
     console.log(json);
