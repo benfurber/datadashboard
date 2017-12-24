6222ba488734cf420328a8fb75700743a9087195
benfurber <ben@furber-consultancy.com>
2017-12-24 21:36:58 +0000

    More node-y-ness in V2

--- js/summary2.js
+++ js/summary2.js
@@ -6,8 +6,8 @@ const stream = require('stream');
 
 const csv = require("csvtojson");
 
-const mainData = '../data/canvas1-processed.csv';
-let mainObject = {};
+const mainData = '../data/neoexports/subscriptions.csv';
+let mainArray = [];
 
 // Secondary data sources that need blending with the main
 let mappingObject = {};
@@ -20,26 +20,82 @@ function addSecondaryItems(name,csvURL) {
 
 addSecondaryItems('completionSource','../data/neoexports/lutsource.csv');
 addSecondaryItems('shiftsSource','../data/neoexports/lutsource.csv');
+addSecondaryItems('knownEmailAddresses','../data/neoexports/knownemailaddresses.csv');
 
 
 // Add completion sources to the mapping object
-csv()
-.fromFile(secondaryItemsList.completionSource)
-.on('json',(jsonObj, rowIndex)=>{
-    mappingObject['completionSource'][jsonObj['Source Desc']] = jsonObj['Source Id']
-  })
-  .on('done',(error)=>{
-      console.log('end')
-      console.log(mappingObject)
-  })
-
+function secondary1() {
+  csv()
+    .fromFile(secondaryItemsList.completionSource)
+    .on('json',(jsonObj)=>{
+      mappingObject['completionSource'][jsonObj['Source Id']] = jsonObj['Source Desc']
+    })
+    .on('done',(error)=>{
+      console.log('Completion sources CSV mapped.')
+    })
+}
 // Add completion sources to the mapping object
-csv()
-.fromFile(secondaryItemsList.shiftsSource)
-.on('json',(jsonObj, rowIndex)=>{
-    mappingObject['shiftsSource'][jsonObj['Source Id']] = jsonObj['Source Desc']
-  })
-  .on('done',(error)=>{
-      console.log('end')
-      console.log(mappingObject)
+function secondary2() {
+  mappingObject['knownEmailAddresses'] = {}
+  csv()
+    .fromFile(secondaryItemsList.knownEmailAddresses)
+    .on('json',(jsonObj) => {
+      if (jsonObj['Email Address'].match(/(@mc.org.uk)/)) {
+        mappingObject['knownEmailAddresses']['emailAddress'] = jsonObj['Email Address'],
+        mappingObject['knownEmailAddresses']['userType'] = 'Staff'
+      } else if (jsonObj['Status'] == ('Loyal' || 'Warm' || 'Staff' )) {
+        mappingObject['knownEmailAddresses']['emailAddress'] = jsonObj['Email Address'],
+        mappingObject['knownEmailAddresses']['userType'] = jsonObj['Status']
+      } else {
+        mappingObject['knownEmailAddresses']['emailAddress'] = jsonObj['Email Address'],
+        mappingObject['knownEmailAddresses']['userType'] = 'Other'
+      }
+    })
+    .on('done',(error)=>{
+      console.log('Known email addresses CSV mapped.')
+      console.log(mappingObject['knownEmailAddresses'])
+    })
+}
+
+Promise.all([secondary1(),secondary2()])
+  .then(function() {
+      csv() // {flatKeys:true}
+      .fromFile(mainData)
+        .on('json',(jsonObj, rowIndex)=>{
+
+          // Creating an empty object. Will be pushed to the array at the end.
+          let item = {}
+
+          // Simple indexing to start.
+          item['objectID'] = rowIndex
+
+          // User ID moved over. Known as RecipientPrimarykey, Unique ID in other places.
+          item['userID'] = jsonObj['RecipientPrimarykey']
+
+          // Mapping the
+          let creationSource = jsonObj['Creation Source']
+          item['creationSource'] = mappingObject['completionSource'][creationSource]
+
+          mainArray.push(item)
+
+        })
+        .on('done',(error)=>{
+            console.log('Finished building main JSON object')
+            console.log(mainArray)
+            mainArray = JSON.stringify(mainArray, null, '\t');
+
+            fs.writeFile("../data/processed/v2.json", mainArray, (err) => {
+
+              // throws an error, you could also catch it here
+              if(err) {
+                  console.log("An error has occured while writing of the collections JSON file.");
+                  console.log(err);
+              };
+
+              // success case, the file was saved
+              console.log('Object written to JSON file.');
+
+            });
+
+        })
   })
